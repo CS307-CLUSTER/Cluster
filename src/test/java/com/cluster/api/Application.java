4abc69c45557d75c69c5b094325ea2cdaa1005de
potatoshane <li2137@purdue.edu>
2017-02-16 19:50:47 -0500

    Added Playground for API Calls Temporarily disabled Spring Session

---
+++ src/test/java/com/cluster/api/Application.java
@@ -0,0 +1,17 @@
+package com.cluster.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by shane on 2/16/17.
+ */
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
