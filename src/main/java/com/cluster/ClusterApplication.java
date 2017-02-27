07ccf9b6f74a59acab9d43d7c67211dfdad5cf7b
potatoshane <li2137@purdue.edu>
2017-02-27 14:56:42 -0500

    Added Facebook OAuth 2.0 Setup Srping Security Settings

--- src/main/java/com/cluster/ClusterApplication.java
+++ src/main/java/com/cluster/ClusterApplication.java
@@ -2,11 +2,22 @@ package com.cluster;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
 @SpringBootApplication
 public class ClusterApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ClusterApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ClusterApplication.class, args);
+    }
+
+    public void configure(HttpSecurity http) throws Exception {
+        http
+                .antMatcher("/**")
+                .authorizeRequests()
+                .antMatchers("/", "/login**", "/webjars/**")
+                .permitAll()
+                .anyRequest()
+                .authenticated();
+    }
 }
