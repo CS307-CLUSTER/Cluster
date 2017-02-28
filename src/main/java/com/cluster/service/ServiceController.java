d85fc782f6efce3303cf79d2e01cbf8637347292
potatoshane <li2137@purdue.edu>
2017-02-28 02:59:31 -0500

    Added code for check if user exists

--- src/main/java/com/cluster/service/ServiceController.java
+++ src/main/java/com/cluster/service/ServiceController.java
@@ -1,14 +1,22 @@
 package com.cluster.service;
 
+import com.cluster.data.DatabaseController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
 import java.security.Principal;
 
 /**
  * Created by shane on 2/28/17.
  */
+
+@Controller
 public class ServiceController {
+    @Autowired
+    private DatabaseController databaseController;
 
     public boolean userExists(long id) {
-        return true;
+        return databaseController.doesUserExist(id);
     }
 
     public boolean createUser(Principal principal, String phoneNumber, String address, String city, String state, String zip) {
