3787d15770ed037ec94a2149fcc1f396ef212115
li2099 <li2099@purdue.edu>
2017-02-28 02:54:39 -0500

    Added Data classes and implemented User functions

--- src/main/java/com/cluster/data/UserCluster.java
+++ src/main/java/com/cluster/data/UserCluster.java
@@ -15,22 +15,22 @@ public class UserCluster {
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
 
-    private Integer user_id;
-    private Integer cluster_id;
+    private int user_id;
+    private int cluster_id;
 
-    public Integer getUser_id() {
+    public int getUser_id() {
         return user_id;
     }
 
-    public void setUser_id(Integer user_id) {
+    public void setUser_id(int user_id) {
         this.user_id = user_id;
     }
 
-    public Integer getCluster_id() {
+    public int getCluster_id() {
         return cluster_id;
     }
 
-    public void setCluster_id(Integer cluster_id) {
+    public void setCluster_id(int cluster_id) {
         this.cluster_id = cluster_id;
     }
 }
