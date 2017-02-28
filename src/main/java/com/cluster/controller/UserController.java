f4b47314ca9247fa12a98387998a31f1ae234631
Andrew Tully <andctully@gmail.com>
2017-02-28 03:16:34 -0500

    Added API calls to create a user and check if a user has already been created

--- src/main/java/com/cluster/controller/UserController.java
+++ src/main/java/com/cluster/controller/UserController.java
@@ -1,11 +1,9 @@
 package com.cluster.controller;
 
+import com.cluster.service.ServiceController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
 
@@ -17,6 +15,9 @@ import java.security.Principal;
 @RequestMapping(path="/user")
 public class UserController {
 
+    @Autowired
+    private ServiceController serviceController;
+
     @GetMapping(path="/test")
     public @ResponseBody String test() {
         return "Test successful!!!!!!!!!!!!!!!!";
@@ -27,9 +28,30 @@ public class UserController {
         return principal.getName();
     }
 
-    @GetMapping(path="/userInfo")
+    @GetMapping(path="/info")
     public @ResponseBody Principal userInfo(Principal principal) {
         return principal;
+    }
+
+    @GetMapping(path="/check")
+    public @ResponseBody String checkUser(Principal principal) {
+        if (serviceController.userExists(Long.parseLong(principal.getName()))) {
+            return "User exists";
+        } else {
+            return "User does not exist";
+        }
+    }
+
+    @GetMapping(path="/create")
+    public @ResponseBody String createUser(Principal principal, @RequestParam String phoneNumber,
+                                           @RequestParam String city, @RequestParam String state,
+                                           @RequestParam String zip, @RequestParam String address) {
+        boolean created = serviceController.createUser(principal, phoneNumber, address, city, state, zip);
+        if (created) {
+            return "User " + principal.getName() + " has been created";
+        } else {
+            return "User " + principal.getName() + " failed to be created";
+        }
     }
 
 }
