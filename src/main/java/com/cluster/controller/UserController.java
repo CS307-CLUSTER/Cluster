c3d95d53f1563cfdc0fc2089d83f350b24bfe405
potatoshane <li2137@purdue.edu>
2017-02-28 02:13:40 -0500

    Added classes for service layer

--- src/main/java/com/cluster/controller/UserController.java
+++ src/main/java/com/cluster/controller/UserController.java
@@ -5,18 +5,31 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
 
 /**
  * Created by shane on 2/27/17.
 */
 
-@Controller
+@RestController
 @RequestMapping(path="/user")
 public class UserController {
 
     @GetMapping(path="/test")
     public @ResponseBody String test() {
         return "Test successful!!!!!!!!!!!!!!!!";
+    }
+
+    @GetMapping(path="/login")
+    public @ResponseBody  String login(Principal principal) {
+        return principal.getName();
+    }
+
+    @GetMapping(path="/userInfo")
+    public @ResponseBody Principal userInfo(Principal principal) {
+        return principal;
     }
 
 }
