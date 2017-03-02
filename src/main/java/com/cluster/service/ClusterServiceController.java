4de7b0edb13017985fb32389b971adbc82ba902c
potatoshane <li2137@purdue.edu>
2017-03-01 19:25:46 -0500

    Added methods for Clutser interaction

---
+++ src/main/java/com/cluster/service/ClusterServiceController.java
@@ -0,0 +1,43 @@
+package com.cluster.service;
+
+import com.cluster.data.DatabaseController;
+import com.cluster.data.Users;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import javax.xml.crypto.Data;
+import java.util.List;
+
+/**
+ * Created by shane on 3/1/17.
+ */
+
+@Controller
+public class ClusterServiceController {
+    @Autowired
+    private DatabaseController databaseController;
+
+    public boolean clusterExists(long id) {
+        return true;
+    }
+
+    public boolean createCluster(int maxUsers, int minUsers, int leaderID, int startTime, int endTime, Location location, String address, String city, String state, String zip) {
+        return true;
+    }
+
+    public Cluster getCluster(long id) {
+        return null;
+    }
+
+    public List<Cluster> getAllClusters() {
+        return null;
+    }
+
+    public boolean addCluster() {
+        return false;
+    }
+
+    public boolean removeCluster() {
+        return false;
+    }
+}
