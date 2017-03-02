4ffa9bce762703322c232e5dcbe24c983e847d4e
potatoshane <li2137@purdue.edu>
2017-03-02 16:58:57 -0500

    Added proper redirect for Facebook Login

--- src/main/resources/static/scripts/controllers/login.js
+++ src/main/resources/static/scripts/controllers/login.js
@@ -8,13 +8,23 @@
  * Controller of yapp
  */
 angular.module('yapp')
-  .controller('LoginCtrl', function($scope, $location) {
+  .controller('LoginCtrl', function ($scope, $location) {
 
-    $scope.submit = function() {
+    $scope.submit = function () {
 
       $location.path('/dashboard');
 
       return false;
     }
 
+  })
+  .controller("home", function ($http) {
+    var self = this;
+    $http.get("/user/test").success(function (data) {
+      self.user = data.userAuthentication.details.name;
+      self.authenticated = true;
+    }).error(function () {
+      self.user = "N/A";
+      self.authenticated = false;
+    });
   });
