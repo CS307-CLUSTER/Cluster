4ce7834a49e0be03a5864f502f40a106f375a375
potatoshane <li2137@purdue.edu>
2017-03-02 15:27:30 -0500

    Changed so redirects to login page instead of Facebook Login

--- src/main/java/com/cluster/ClusterApplication.java
+++ src/main/java/com/cluster/ClusterApplication.java
@@ -4,25 +4,32 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @SpringBootApplication
 @EnableOAuth2Sso
-public class ClusterApplication {
+public class ClusterApplication extends WebSecurityConfigurerAdapter {
 
     public static void main(String[] args) {
         SpringApplication.run(ClusterApplication.class, args);
     }
 
-    public void configure(HttpSecurity http) throws Exception {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
         http
                 .antMatcher("/**")
-                .authorizeRequests()
-                    .antMatchers("/", "/login**", "/webjars/**")
-                    .permitAll()
-                .anyRequest()
-                    .authenticated()
-        .and().logout().logoutSuccessUrl("/test.html").permitAll()
-        .and().csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+                    .authorizeRequests()
+                        .antMatchers("/",
+                                "/login**",
+                                "/webjars/**",
+                                "/bower_components/**",
+                                "/scripts/**",
+                                "/styles/**",
+                                "/views/**",
+                                "/images/**")
+                        .permitAll()
+                    .anyRequest()
+                        .authenticated();
     }
 }
