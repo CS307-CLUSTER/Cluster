ee732375600e6da4af2aa63e5fb24b49ef618026
Andrew Tully <andctully@gmail.com>
2017-03-02 20:58:10 -0500

    Added dummy API calls to create a cluster and to get a cluster's information

--- src/main/java/com/cluster/controller/ClusterController.java
+++ src/main/java/com/cluster/controller/ClusterController.java
@@ -1,11 +1,11 @@
 package com.cluster.controller;
 
-import com.cluster.service.UserService;
+import com.cluster.service.Cluster;
+import com.cluster.service.ClusterService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
 
 /**
  * Created by Andrew on 2/28/17.
@@ -16,18 +16,26 @@ import org.springframework.web.bind.annotation.RestController;
 public class ClusterController {
 
     @Autowired
-    private UserService serviceController;
+    private ClusterService serviceController;
 
     @GetMapping(path="/create")
-    public @ResponseBody String createCluster() {
-        // TODO Determine parameters from frontend
-        // TODO Implement creation from serviceController it has been implemented by @ShaneLi
-        return "Cluster creation reached"; // temporary dummy response
+    public @ResponseBody String createCluster(Principal principal, @RequestParam int maxUsers,
+                                              @RequestParam int minUsers, @RequestParam int leaderID,
+                                              @RequestParam int startTime, @RequestParam int endTime,
+                                              @RequestParam String address, @RequestParam String city,
+                                              @RequestParam String state, @RequestParam String zip) {
+        boolean created = serviceController.createCluster(maxUsers, minUsers, leaderID, startTime,
+                endTime, address, city, state, zip);
+        if (created) {
+            return "Cluster created!";
+        } else {
+            return "Cluster failed to be created";
+        }
     }
 
     @GetMapping(path="/info")
-    public @ResponseBody String clusterInfo() {
+    public @ResponseBody Cluster clusterInfo(Principal principal, @RequestParam long id) {
         // TODO Determine parameters
-        return "Cluster information reached"; // temporary dummy response
+        return serviceController.getCluster(id);
     }
 }
