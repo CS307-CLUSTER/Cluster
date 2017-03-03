c793dbe711e8d8ef60b52f2ba39a262c833027f2
potatoshane <li2137@purdue.edu>
2017-03-03 16:25:55 -0500

    Added join cluster functionality

--- src/main/resources/static/scripts/controllers/clusters.js
+++ src/main/resources/static/scripts/controllers/clusters.js
@@ -8,7 +8,7 @@
  * Controller of yapp
  */
 angular.module('yapp')
-  .controller('ClustersCtrl', function ($scope, $http, $location) {
+  .controller('ClustersCtrl', function ($scope, $http, $location, $state) {
     $http.get('/cluster/getAll')
       .then(function (response) {
         $scope.groups = response.data;
@@ -30,7 +30,7 @@ angular.module('yapp')
     $scope.submit = function () {
       console.log("I've ran");
       formData = $scope.form;
-      $location.path('/dashboard');
+      $location.path('/dashboard/clusters');
       var url = ('/cluster/create?maxUsers=' + formData.maxUsers + '&minUsers=' + formData.minUsers + '&startTime='
       + formData.startTime + '&endTime=' + formData.endTime + '&address=' + formData.address1 + '&city='
       + formData.city + '&state=' + formData.state + '&zip=' + formData.zip);
@@ -41,7 +41,24 @@ angular.module('yapp')
         console.log('Could not create cluster');
       });
       return false;
+    };
+
+    $scope.join = function(clusterId) {
+      var url = ('cluster/join?clusterId=' + clusterId);
+      console.log("Join function ran on clusterId: " + clusterId);
+      $http.get(url).success(function (response) {
+        if (response) {
+          console.log("Added user to cluster " + clusterId);
+        } else {
+          alert("Could not join cluster! Check if you are already in one!");
+        }
+      }).error(function () {
+          console.log("hmmm... something went wrong with adding user to cluster. Check console!");
+      });
+      $state.reload();
     }
+
+
   });
 
 
