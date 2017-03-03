9b9f21e6be282d046bb87ac28e06c6307f47b263
shaimohan23 <mohan22@purdue.edu>
2017-03-03 01:35:33 -0500

    Implementing createUser for frontend.

--- src/main/resources/static/scripts/controllers/login.js
+++ src/main/resources/static/scripts/controllers/login.js
@@ -27,4 +27,45 @@ angular.module('yapp')
       self.user = "N/A";
       self.authenticated = false;
     });
+  })
+  .controller("UserCreationCtrl", function ($scope, $http) {
+    var data = 'parameters',config='contenttype',
+      phoneNumber = document.getElementsByName("Phone Number")[0].value,
+      city = document.getElementsByName("City")[0].value,
+      state = document.getElementsByName("State")[0].value,
+      zipCode = document.getElementsByName("Zip Code")[0].value,
+      address = document.getElementsByName("Address")[0].value,
+      url = ('localhost:8080/user/create?phoneNumber=' + phoneNumber + '&city=' + city + '&state=' + state + '&zip=' + zipCode + '&address=' + address);
+      var isCreated = $http.get(url)
+      .then(function(response) {
+        //$scope.data = response.data;
+        alert(isCreated);
+      })
   });
+
+  /*function uPost() {
+    var parameter,
+      data = 'parameters',config='contenttype',
+      phoneNumber = document.getElementsByName("Phone Number")[0].value,
+      city = document.getElementsByName("City")[0].value,
+      state = document.getElementsByName("State")[0].value,
+      zipCode = document.getElementsByName("Zip Code")[0].value,
+      address = document.getElementsByName("Address")[0].value,
+      url = ('localhost:8080/user/create?phoneNumber=' + phoneNumber + '&city=' + city + '&state=' + state + '&zip=' + zipCode + '&address=' + address);
+
+      parameter = JSON.stringify({type:"text", phoneNumber: phoneNumber, city: city, state: state, zipCode: zipCode, address: address});
+      var response = $http.get(url, parameter).
+      success(function(data, status, headers, config) {
+        console.log(data);
+      }).
+      error(function(data, status, headers, config) {
+      });
+    }*/
+
+  var subButton = document.getElementById('subButton');
+  subButton.addEventListener('click', uPost, false);
+
+
+
+
+//'localhost:8080/user/create?phoneNumber=1234&city=Palo Alto&state=California&zip=94306&address=680 Georgia Ave'
