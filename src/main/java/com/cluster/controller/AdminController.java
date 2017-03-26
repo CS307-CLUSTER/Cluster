97aee9aede2e125fc55c53984d6dea42cb19fe86
Andrew Tully <andctully@gmail.com>
2017-03-25 20:05:08 -0400

    Added API routes in the admin controller class to get number of active users and clusters as well as disband clusters and get a users information

--- src/main/java/com/cluster/controller/AdminController.java
+++ src/main/java/com/cluster/controller/AdminController.java
@@ -1,29 +1,66 @@
 package com.cluster.controller;
 
 import com.cluster.service.AdminService;
+import com.cluster.service.User;
+import com.cluster.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
 
 /**
- * Created by Andrew on 3/12/17.
+ * Created by Andrew
  */
 @RestController
 @RequestMapping(path="admin")
 public class AdminController {
 
-    @Autowired
-    private AdminService adminService;
+    @Autowired private AdminService adminService;
+    @Autowired private UserService userService;
 
-    @GetMapping(path="/numActiveUsers")
+    @GetMapping(path = "/getNumActiveUsers")
     public @ResponseBody int getNumActiveUsers(Principal principal) {
         // verify user is admin
-        return adminService.getNumActiveUsers(principal);
+        User user = userService.getUserFromDatabase(Long.parseLong(principal.getName()));
+        if (user.isAdmin()) {
+            return adminService.getNumActiveUsers();
+        } else {
+            return -1; // user was not an admin
+        }
     }
 
+    @GetMapping(path = "/getNumActiveClusters")
+    public @ResponseBody int getNumActiveClusters(Principal principal) {
+        // verify user is admin
+        User user = userService.getUserFromDatabase(Long.parseLong(principal.getName()));
+        if (user.isAdmin()) {
+            return adminService.getNumActiveClusters();
+        } else {
+            return -1; // user was not an admin
+        }
+    }
+
+    @GetMapping(path = "disbandCluster")
+    public @ResponseBody boolean getNumActiveUsers(Principal principal, @RequestParam long clusterId) {
+        // verify user is admin
+        User user = userService.getUserFromDatabase(Long.parseLong(principal.getName()));
+        if (user.isAdmin()) {
+            return adminService.disbandCluster(clusterId);
+        } else {
+            return false;
+        }
+    }
+
+    @GetMapping(path = "getUserInformation")
+    public @ResponseBody User getUserInformation(Principal principal, @RequestParam int userId) {
+        // verify user is admin
+        User user = userService.getUserFromDatabase(Long.parseLong(principal.getName()));
+        if (user.isAdmin()) {
+            return adminService.getUserInformation(userId);
+        } else {
+            return null;
+        }
+    }
 
 }
+
