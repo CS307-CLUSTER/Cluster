8679e5f572d225bd98c841c60ddb05c1264b8cce
potatoshane <li2137@purdue.edu>
2017-04-05 03:05:30 -0400

    Added edit function for My Profile

--- src/main/resources/static/scripts/controllers/profile.js
+++ src/main/resources/static/scripts/controllers/profile.js
@@ -10,9 +10,58 @@
 angular.module('yapp')
   .controller('ProfileCtrl', function ($scope, $http) {
     $http.get('/user/info')
-      .then(function(response) {
+      .then(function (response) {
         $scope.data = response.data;
       });
+  })
+  .directive("clickToEdit", function () {
+    var editorTemplate = '' +
+      '<div class="container">' +
+      '<div ng-hide="view.editorEnabled">' +
+      '{{value}} ' +
+      '<a class="btn btn-primary btn-outline btn-rounded" ng-click="enableEditor()">Edit</a>' +
+      '</div>' +
+      '<div ng-show="view.editorEnabled">' +
+      '<input type="text" class="small-12.columns" ng-model="view.editableValue">' +
+      '<a class="btn btn-primary btn-outline btn-rounded" href="#" ng-click="save()">Save</a>' +
+      ' or ' +
+      '<a class="btn btn-primary btn-outline btn-rounded" ng-click="disableEditor()">cancel</a>' +
+      '</div>' +
+      '</div>';
+
+    return {
+      restrict: "A",
+      replace: true,
+      template: editorTemplate,
+      scope: {
+        value: "=clickToEdit",
+      },
+      link: function (scope, element, attrs) {
+        scope.view = {
+          editableValue: scope.value,
+          editorEnabled: false
+        };
+
+        scope.enableEditor = function () {
+          scope.view.editorEnabled = true;
+          scope.view.editableValue = scope.value;
+          setTimeout(function () {
+            element.find('input')[0].focus();
+            //element.find('input').focus().select(); // w/ jQuery
+          });
+        };
+
+        scope.disableEditor = function () {
+          scope.view.editorEnabled = false;
+        };
+
+        scope.save = function () {
+          scope.value = scope.view.editableValue;
+          scope.disableEditor();
+        };
+
+      }
+    };
   });
 
 console.log("Im being run");
