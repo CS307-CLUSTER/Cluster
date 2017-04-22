df998c1b8182271ddc22bcb43a6404fbc6648a08
potatoshane <li2137@purdue.edu>
2017-04-22 19:03:17 -0400

    Added Modal

--- src/main/resources/static/scripts/controllers/other-profile.js
+++ src/main/resources/static/scripts/controllers/other-profile.js
@@ -1,4 +1,3 @@
-
 'use strict';
 
 /**
@@ -14,4 +13,50 @@ angular.module('yapp')
       .then(function (response) {
         $scope.data = response.data;
       });
+    $scope.showModal = false;
+    $scope.buttonClicked = "";
+    $scope.toggleModal = function (btnClicked) {
+      $scope.buttonClicked = btnClicked;
+      $scope.showModal = !$scope.showModal;
+    };
+  })
+
+  .directive('modal', function () {
+    return {
+      template: '<div class="modal fade">' +
+      '<div class="modal-dialog">' +
+      '<div class="modal-content">' +
+      '<div class="modal-header">' +
+      '<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>' +
+      '<h4 class="modal-title">{{ buttonClicked }} clicked!!</h4>' +
+      '</div>' +
+      '<div class="modal-body" ng-transclude></div>' +
+      '</div>' +
+      '</div>' +
+      '</div>',
+      restrict: 'E',
+      transclude: true,
+      replace: true,
+      scope: true,
+      link: function postLink(scope, element, attrs) {
+        scope.$watch(attrs.visible, function (value) {
+          if (value == true)
+            $(element).modal('show');
+          else
+            $(element).modal('hide');
+        });
+
+        $(element).on('shown.bs.modal', function () {
+          scope.$apply(function () {
+            scope.$parent[attrs.visible] = true;
+          });
+        });
+
+        $(element).on('hidden.bs.modal', function () {
+          scope.$apply(function () {
+            scope.$parent[attrs.visible] = false;
+          });
+        });
+      }
+    };
   });
