4520bd3bf889e2070268bc8558616bbe8c26508c
DavidTong <tongd@purdue.edu>
2017-04-22 19:39:33 -0400

    Improved admin page layout

--- src/main/resources/static/scripts/controllers/admin.js
+++ src/main/resources/static/scripts/controllers/admin.js
@@ -17,6 +17,18 @@ angular.module('yapp')
       .then(function (response) {
         $scope.activeClusters = response.data;
       });
+    $http.get('/admin/getNumCompletedClusters')
+      .then(function (response) {
+        $scope.numCompleted = response.data;
+      });
+    $http.get('/admin/getNumNotCompletedClusters')
+      .then(function (response) {
+        $scope.numNotCompleted = response.data;
+      });
+    $http.get('/user/getAll')
+      .then(function (response) {
+        $scope.users = response.data;
+      });
     $http.get('/cluster/getAll')
       .then(function (response) {
         $scope.groups = response.data;
@@ -32,6 +44,21 @@ angular.module('yapp')
         }
       }).error(function () {
         console.log("Something went wrong with disbanding the cluster. Check the console!");
+        return false;
+      });
+    };
+
+    $scope.view = function (userId) {
+      var url = ('admin/getUserInformation?userId=' + userId);
+      console.log(url);
+      $http.get(url).success(function (response) {
+        if (response) {
+          $state.reload();
+        } else {
+          alert("Could not view profile!");
+        }
+      }).error(function () {
+        console.log("Something went wrong with viewing profile. Check the console!");
         return false;
       });
     };
