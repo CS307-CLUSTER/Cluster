7dbde3269e7ffb5ffff0af3292dbd22c188bb99c
potatoshane <li2137@purdue.edu>
2017-04-23 19:38:53 -0400

    Added Modal for View Profile in Admin

--- src/main/resources/static/scripts/controllers/other-profile.js
+++ src/main/resources/static/scripts/controllers/other-profile.js
@@ -28,7 +28,7 @@ angular.module('yapp')
       '<div class="modal-content">' +
       '<div class="modal-header">' +
       '<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>' +
-      '<h4 class="modal-title">{{ buttonClicked }} clicked!!</h4>' +
+      '<h4 class="modal-title">{{ buttonClicked }}</h4>' +
       '</div>' +
       '<div class="modal-body" ng-transclude></div>' +
       '</div>' +
